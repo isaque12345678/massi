865dd75e1f25fefdadff6cc9ff6f2d54a194ca5b
isaque12345678 <163149728+isaque12345678@users.noreply.github.com>
2024-03-12 11:26:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://images.ctfassets.net/3mv54pzvptwz/7j7SKmVPFntXV7XZZbmAd1/44f225126f47f7ef140d06d06aa8b050/24SS_Social_TS_Football_Q1-Phenomenal-Pack_Future_Neymar_Portrait_holding_boot_3633__4x5_1080x1350px.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https//diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2955269:1592145699/bob-esponja-1367318.jpg?f=16x9&h=574&w=1020&$p$f$h$w=a";
+
+}
